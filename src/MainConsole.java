767ab9b0fe2ac19c9a4effbc8655ecfef44d8dbf
morabarna <morabarna16@gmail.com>
2023-02-20 13:07:56 +0100

    input and calc done

--- src/MainConsole.java
+++ src/MainConsole.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class MainConsole {
     
+    public MainConsole() {
+        double[] input = input();
+        Telteg calc = new Telteg();
+        calc.calcArea(input);
+    }
+
+    public double[] input(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Adja meg az A oldalt: ");
+        double sideA = scanner.nextDouble();
+        System.out.println("Adja meg a B oldalt: ");
+        double sideB = scanner.nextDouble();
+        double[] input = {sideA,sideB};
+        return input;
+    }
 }
--- src/Telteg.java
+++ src/Telteg.java
@@ -1,3 +1,6 @@
 public class Telteg {
-    
+    double calcArea(double[] input){
+        double area = 1.0*(input[0]*input[1])*2;
+        return area;
+    }
 }
